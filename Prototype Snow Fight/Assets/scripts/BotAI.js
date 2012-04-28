b36efdb46a022f457afb5aee22b48737289800fd
ben <Ben@KeksKlein.(none)>
2012-04-28 16:48:38 +0200

    bot prefab angefangen. (greift figuren anderen teams an. dafuer aenderungen in projectile. fpscontroller durch daraus abgeleiteten inputcontroller ersetzt)

---
+++ Prototype Snow Fight/Assets/scripts/BotAI.js
@@ -0,0 +1,195 @@
+var attackTurnTime = 0.7;
+var rotateSpeed = 120.0;
+var attackDistance = 17.0;
+var extraRunTime = 2.0;
+var damage = 1;
+
+var attackSpeed = 5.0;
+var attackRotateSpeed = 20.0;
+
+var idleTime = 1.6;
+
+var punchRadius = 7.1;
+
+private var attackAngle = 10.0;
+private var isAttacking = false;
+
+var target : Transform;
+
+// Cache a reference to the motor
+private var motor : CharacterMotor;
+motor = GetComponent(CharacterMotor);
+
+private var pStatus : PlayerStatus;
+pStatus = GetComponent(PlayerStatus);
+
+var bulletSpawn : Projectile;
+
+function Start ()
+{
+	
+	yield WaitForSeconds(Random.value);
+	
+	// Just attack for now
+	while (true)	
+	{
+		// Don't do anything when idle. And wait for player to be in range!
+		// This is the perfect time for the player to attack us
+		yield Idle();
+
+		// Prepare, turn to player and attack him
+		yield Attack();
+	}
+}
+
+
+function Idle ()
+{
+	
+	// Don't do anything when idle
+	// The perfect time for the player to attack us
+	yield WaitForSeconds(idleTime);
+
+	// And if the player is really far away.
+	// We just idle around until he comes back
+	// unless we're dying, in which case we just keep idling.
+	while (true)
+	{
+		motor.inputMoveDirection = Vector3.zero;
+		yield WaitForSeconds(0.2);
+		
+		var tar = FindClosestEnemy();
+		if (tar != null) {
+			target = tar.transform;
+			var offset = transform.position - target.position;
+			// if player is in range again, stop lazyness
+			// Good Hunting!		
+			if (offset.magnitude < attackDistance)
+				return;
+		}
+	}
+} 
+
+function RotateTowardsPosition (targetPos : Vector3, rotateSpeed : float) : float
+{
+	// Compute relative point and get the angle towards it
+	var relative = transform.InverseTransformPoint(targetPos);
+	var angle = Mathf.Atan2 (relative.x, relative.z) * Mathf.Rad2Deg;
+	// Clamp it with the max rotation speed
+	var maxRotation = rotateSpeed * Time.deltaTime;
+	var clampedAngle = Mathf.Clamp(angle, -maxRotation, maxRotation);
+	// Rotate
+	transform.Rotate(0, clampedAngle, 0);
+	// Return the current angle
+	return angle;
+}
+
+function Attack ()
+{
+	isAttacking = true;
+	
+	// First we wait for a bit so the player can prepare while we turn around
+	// As we near an angle of 0, we will begin to move
+	var angle : float;
+	angle = 180.0;
+	var time : float;
+	time = 0.0;
+	var direction : Vector3;
+	while (angle > 5 || time < attackTurnTime)
+	{
+		time += Time.deltaTime;
+		angle = Mathf.Abs(RotateTowardsPosition(target.position, rotateSpeed));
+		move = Mathf.Clamp01((90 - angle) / 90);
+		
+		// depending on the angle, start moving
+		direction = transform.TransformDirection(Vector3.forward * attackSpeed * move);
+		motor.inputMoveDirection = Vector3.zero;
+		
+		yield;
+	}
+	
+	// Run towards player
+	var timer = 0.0;
+	var lostSight = false;
+	while (timer < extraRunTime)
+	{
+		angle = RotateTowardsPosition(target.position, attackRotateSpeed);
+			
+		// The angle of our forward direction and the player position is larger than 50 degrees
+		// That means he is out of sight
+		if (Mathf.Abs(angle) > 40)
+			lostSight = true;
+			
+		// If we lost sight then we keep running for some more time (extraRunTime). 
+		// then stop attacking 
+		if (lostSight)
+			timer += Time.deltaTime;	
+		
+		// Just move forward at constant speed
+		direction = transform.TransformDirection(Vector3.forward * attackSpeed);
+
+		// Keep looking if we are hitting our target
+		// If we are, knock them out of the way dealing damage
+		var pos = transform.position;
+		if((pos - target.position).magnitude < punchRadius)
+		{
+			bulletSpawn.inputFire = true;
+			direction = Vector3.zero;
+		}
+		
+		motor.inputMoveDirection = direction;
+		
+		// We are not actually moving forward.
+		// This probably means we ran into a wall or something. Stop attacking the player.
+//		if (motor.movement.velocity.magnitude < attackSpeed * 0.3)
+//			break;
+		
+		// yield for one frame
+		yield;
+	}
+	
+	
+
+	isAttacking = false;
+	motor.inputMoveDirection = Vector3.zero;
+	
+}
+
+function ApplyDamage ()
+{
+	
+}
+
+function OnDrawGizmosSelected ()
+{
+	Gizmos.color = Color.yellow;
+	Gizmos.DrawWireSphere (transform.position, punchRadius);
+	Gizmos.color = Color.red;
+	Gizmos.DrawWireSphere (transform.position, attackDistance);
+}
+
+// Find the name of the closest enemy within distance
+function FindClosestEnemy () : GameObject {
+    // Find all game objects with tag Enemy
+    var gos : GameObject[];
+    gos = GameObject.FindGameObjectsWithTag("Player"); 
+    var closest : GameObject;
+    var distance = Mathf.Infinity; 
+    var position = transform.position; 
+    // Iterate through them and find the closest one
+    for (var go : GameObject in gos)  { 
+    	var status = go.GetComponent(PlayerStatus);
+    	if (status != null && status.teamNumber != pStatus.teamNumber) {
+	        var diff = (go.transform.position - position);
+	        var curDistance = diff.sqrMagnitude; 
+	        if (curDistance < distance) { 
+	            closest = go; 
+	            distance = curDistance; 
+	        } 
+        }
+    } 
+    return closest;    
+}
+
+
+@script RequireComponent (CharacterMotor)
--- Prototype Snow Fight/Assets/scripts/Projectile.js
+++ Prototype Snow Fight/Assets/scripts/Projectile.js
@@ -1,14 +1,21 @@
 var projectile : Rigidbody;
 var damage = 5;
 
-function Update () {
+var reloadProgress = 0;
 
-	if(Input.GetButtonDown("Fire1")){
-	    Spawnpoint = transform.Find("/Player/BulletSpawnPoint");	
+@System.NonSerialized
+var inputFire : boolean = false;
+
+function Update () {
+	reloadProgress -= Time.deltaTime;
+	if(inputFire && reloadProgress <= 0){
+	    Spawnpoint = transform;
 		SpawnVector = Vector3(Spawnpoint.position.x, Spawnpoint.position.y, Spawnpoint.position.z);	
 		var clone : Rigidbody;	
 		clone = Instantiate(projectile, SpawnVector, Spawnpoint.rotation);
 		
 		clone.velocity = Spawnpoint.TransformDirection (SpawnVector.forward*20);
+		inputFire = false;
+		reloadProgress = 50;
 	}
 }
