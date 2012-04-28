ba986bbfa1a638512d9d996581c8850e362cda54
Tiare Feuchtner <tiaref26@gmail.com>
2012-04-29 01:55:57 +0200

    Modify respawn

--- Prototype Snow Fight/Assets/scripts/PlayerStatus.js
+++ Prototype Snow Fight/Assets/scripts/PlayerStatus.js
@@ -2,23 +2,31 @@ var teamNumber = 1;
 var teamBase : Transform;
 var fullHp : int = 50;
 var stunDuration = 3.0;
+var respawnTimeout = 3.0;
 
 private var hp : int = fullHp;
-private var isControllable = true;
 private var stunTime;
+private var killTime;
 private var gameState : GameStatus;
+private var died : boolean = false;
+private var stunned : boolean = false;
 //InvokeRepeating("Regenerate",5,5);
 //var damageSound : AudioClip;
 
 
 function Update () {
-  if(hp==0){
-  	//audio.PlayOneShot(damageSound);
-  	gameObject.GetComponent("Detonator").Explode();
-  }
+//  if(hp==0){
+//  	//audio.PlayOneShot(damageSound);
+//  	gameObject.GetComponent("Detonator").Explode();
+//  }
   
-  if(!isControllable && Time.time > stunTime + stunDuration)
-  	isControllable = true;
+  if(stunned && Time.time > stunTime + stunDuration) {
+  	stunned = false;
+  	GetComponent(CharacterMotor).canControl = true;
+	}
+  	
+	if (died && Time.time > killTime + respawnTimeout)
+		Respawn();
 }
 
 //function Regenerate () {
@@ -44,20 +52,27 @@ function OnCollisionEnter (collision : Collision) {
 }
 
 function Die (ball : Damage) {
+	if (died) //we're already dead
+		return;
 	gameState = GameObject.FindWithTag("Game").GetComponent("GameStatus");
 	gameState.IncreaseScore(ball.GetShootingTeam());
 	
-	hp = fullHp;
-	Respawn ();
+	died = true;
+	killTime = Time.time;
 }
 
 function Stun () {
-	if(isControllable) {
-		isControllable = false;
-		stunTime = Time.time;
+	if(GetComponent(CharacterMotor).canControl) {
+		GetComponent(CharacterMotor).canControl = false;
 	}
+	stunned = true;
+	stunTime = Time.time;
 }
 
 function Respawn () {
 	transform.position = teamBase.position;
+	hp = fullHp;
+	died = false;
+	
+	Stun();
 }
