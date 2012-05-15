6d7be680049dd2f083d94f6487329170a0de3184
Hannes Rammer <Monstars99@googlemail.com>
2012-05-15 20:06:10 +0200

    added resetElement

--- eleMan/Assets/hannes/EleManStats.js
+++ eleMan/Assets/hannes/EleManStats.js
@@ -1,15 +1,16 @@
 var element : String;
 ///initial values stored at start to recover overwritten stats by elements
 private var initialElement : String;
-private var initialJumpHeight : int;
-private var initialWalkSpeed : int;
-private var initialRunSpeed : int;
+private var initialJumpHeight : float;
+private var initialWalkSpeed :float;
+private var initialRunSpeed :float;
 private var player : ThirdPersonController;
 
+
 function Start() {
-	player = gameObject.GetComponent("Third Person Controller");
+	player = gameObject.GetComponent("ThirdPersonController");
 	element = "Normal";
-	initialJumpHeight =  12;//player.jumpHeight;
+	initialJumpHeight = player.jumpHeight;
 	// The speed when walking
 	initialWalkSpeed = player.walkSpeed;
 	// when pressing "Fire3" button (cmd) we start running
