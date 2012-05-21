d025270888613504a8279e063422dcbfae54b418
ben <Ben@KeksKlein.(none)>
2012-05-21 22:05:25 +0200

    materialien

--- eleMan/Assets/Scripts/Level/ElementCreation.js
+++ eleMan/Assets/Scripts/Level/ElementCreation.js
@@ -1,11 +1,8 @@
 var currentElement : String;
-
 var airElement : Transform;
 var earthElement : Transform;
 var fireElement : Transform;
 var waterElement : Transform;
-
-private var layDownProgress = 0;
 
 function Start () {
 	currentElement = "";
@@ -13,27 +10,16 @@ function Start () {
 
 function Update () {
 	//We must move the spawn Point a little bit away from the player.
-	layDownProgress -= Time.deltaTime;
-	if (!currentElement.Equals("") && layDownProgress <= 0) {
-		layDownProgress = 100;
-		Spawnpoint = transform;
-		SpawnVector = Vector3(Spawnpoint.position.x, Spawnpoint.position.y, Spawnpoint.position.z);
-		var clone : Transform;
+	if (!currentElement.Equals("")) {
 		if (currentElement.Equals("water")) {
-			clone = Instantiate(waterElement);
 		} else if (currentElement.Equals("fire")) {
-			clone = Instantiate(fireElement);
 		} else if (currentElement.Equals("air")) {
-			clone = Instantiate(airElement);
 		} else if (currentElement.Equals("earth")) {
-			clone = Instantiate(earthElement);
 		}
-		currentElement = "";
-		layDownProgress = 100;
 	}
 }
 
 //Set the element because we need to create the right element when pressing Ctrl.
-function SetElement(element : String) {
+function setElement(element : String) {
 	currentElement = element;
 }
