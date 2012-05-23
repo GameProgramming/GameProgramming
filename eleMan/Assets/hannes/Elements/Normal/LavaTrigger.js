46f362cf9eae25dee263c68c74f5041ff937ab2d
Hannes Rammer <Monstars99@googlemail.com>
2012-05-23 18:52:30 +0200

    added lava

---
+++ eleMan/Assets/hannes/Elements/Normal/LavaTrigger.js
@@ -0,0 +1,28 @@
+
+var textDisplay : GUIText;
+
+
+function Start() {
+    if(GameObject.Find("Text Display")) {
+	    textDisplay = GameObject.Find("Text Display").guiText;
+	    textDisplay.text = "";
+    } 
+}
+
+
+function OnTriggerEnter(col : Collider) {
+	
+	if (col.gameObject.tag == "Player") {
+		player = col.gameObject;
+		//if(player.GetComponent("EleManStats").element == "normal")
+		//{
+    	    //reset values that might habe been changed by other elements
+    	 	player.GetComponent("EleManStats").SetElement("normal");
+    		player.GetComponent("PlayerStatus").Spawn();
+        //}
+        
+        textDisplay.text = player.GetComponent("EleManStats").element + "PlayerHitLava";
+		//print("hit");
+        
+    }
+}
