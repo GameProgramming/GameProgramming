60af0fcf1d117d978ca201a5687114fafda4eaf1
Andreas Buescher <andi_buescher0404@gmx.de>
2012-05-30 19:06:44 +0200

    Added flags

--- SnowFight/Assets/Map/TeamBase.js
+++ SnowFight/Assets/Map/TeamBase.js
@@ -1,8 +1,11 @@
 #pragma strict
 
 private var enterTime = 0.0;
-private var team :Team;
+private var enterTimePlayer = 0.0;
+private var team : Team;
+private var color : Color;
 var points : int = 3;
+
 
 function Start () {
 	team = transform.parent.gameObject.GetComponent(Team);
@@ -27,8 +30,26 @@ function OnTriggerStay(other : Collider) {
 			enterTime = 0.0;
 		}
 	}
+	
+	//Check if player.
+	//If yes we want to check if he stays for 5 seconds inside the base 
+	if (other.tag.Equals("Player")) {
+		enterTimePlayer += Time.deltaTime;
+		if (enterTimePlayer > 5.0) {
+			enterTimePlayer = 0.0;
+			var flagColor = transform.parent.GetComponentInChildren(TeamFlagColor).GetColor();
+			if (flagColor == Color.gray) {
+				transform.parent.GetComponentInChildren(TeamFlagColor).SetColor(Color.blue);
+			}
+			if (flagColor == Color.blue || flagColor == Color.red) {
+				transform.parent.GetComponentInChildren(TeamFlagColor).SetColor(Color.gray);
+			}
+		}
+	}
 }
 
-function OnTriggerExit() {
-	enterTime = 0.0;
+function OnTriggerExit(other : Collider) {
+	if (other.tag.Equals("Player")) {
+		enterTimePlayer = 0.0;
+	}
 }
