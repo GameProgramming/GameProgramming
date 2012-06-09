a9e45c47f6c9b971a854e209dfee8d8846d7a3d1
Tiare Feuchtner <tiaref26@gmail.com>
2012-06-10 01:21:11 +0200

    Add  ItemManager and restructure code.

---
+++ SnowFight/Assets/Weapons/ItemManager.js
@@ -0,0 +1,66 @@
+#pragma strict
+private var motor : CharacterMotorSF;
+private var pStatus : PlayerStatus;
+
+@System.NonSerialized
+var inputAction : boolean = false;
+
+private var item : GameObject;
+private var candidateItem : GameObject;
+private var movementOffset : Vector3;
+var maxCandidateDistance : float = 1.0;
+
+function Start () {
+	motor = GetComponent(CharacterMotorSF);
+	pStatus = GetComponent(PlayerStatus);
+	item = null;
+}
+
+function Update () {
+	//player releases action button or dies
+	if (item && (inputAction || pStatus.IsDead())) {
+		item.SendMessage("Release", null, SendMessageOptions.DontRequireReceiver);
+		ReleaseItem();
+	}
+
+	else if (inputAction && candidateItem && !CandidateTooFarAway()) {//TODO: also check if nobody else has the ball etc.
+		item = candidateItem;
+		item.SendMessage("PickItem", gameObject, SendMessageOptions.DontRequireReceiver);
+	}
+}
+
+function OnControllerColliderHit (hit : ControllerColliderHit) {
+	if (hit.gameObject.layer == LayerMask.NameToLayer("Item")) {
+		candidateItem = hit.gameObject;
+	}
+}
+
+function PassOnMovementOffset (offset : Vector3) {
+
+	if (item) 
+		item.SendMessage("Move", offset, SendMessageOptions.DontRequireReceiver);
+}
+
+function ReleaseItem () {
+	if(item) {
+		item = null;
+	}
+}
+
+function CandidateTooFarAway() {
+	var totalDistance = GetComponent(CharacterController).radius + candidateItem.collider.bounds.size.x + maxCandidateDistance;
+	if (Vector3.Distance(candidateItem.transform.position, transform.position) > totalDistance) {
+		candidateItem = null;
+		item = null;
+		return true;
+	}
+	else
+		return false;
+}
+
+
+function GetItem () {
+	return item;
+}
+
+@script RequireComponent (CharacterMotorSF)
