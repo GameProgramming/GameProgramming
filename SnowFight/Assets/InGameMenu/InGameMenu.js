37da4e0657888775411a6d525b5f2a84711047f0
Andreas Buescher <andi_buescher0404@gmx.de>
2012-06-20 22:28:15 +0200

    In Game Menu

--- SnowFight/Assets/InGameMenu/InGameMenu.js
+++ SnowFight/Assets/InGameMenu/InGameMenu.js
@@ -1,8 +1,25 @@
+private var showIngameMenu : boolean;
+
 
 function Start () {
+	showIngameMenu = false;
+}
 
+function Update() {
+	if (Input.GetKeyDown(KeyCode.Escape)) {
+		showIngameMenu = !showIngameMenu;
+	}
 }
 
 function OnGUI () {
 
+	if (showIngameMenu) {
+		GUI.Box(Rect (100, 100, 300, 300), "Menu");
+		if (GUI.Button (Rect (110, 110, 200, 50), "Exit Game")) {
+			Application.Quit();
+		}
+		if (GUI.Button (Rect (110, 170, 200, 50), "Restart Level")) {
+			Application.LoadLevel("Main");
+		}
+	}
 }
