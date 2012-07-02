9a14d06f747b7dde4498481900873177b9b6c8a7
Andreas Buescher <andi_buescher0404@gmx.de>
2012-07-02 17:06:44 +0200

    Added change map function

--- SnowFight/Assets/InGameMenu/InGameMenu.js
+++ SnowFight/Assets/InGameMenu/InGameMenu.js
@@ -1,30 +1,40 @@
 private var showIngameMenu : boolean;
+private var showChangeMap : boolean;
 private var ingameMenuRect;
+private var changeMapRect;
 private var locked : boolean = false;
 private var pressed : boolean = false;
 
 function Awake () {
 	showIngameMenu = false;
+	showChangeMap = false;
 	ingameMenuRect = Rect(Screen.width/2 - 125, Screen.height/2 - 75, 250, 150);
+	changeMapRect = Rect(Screen.width/2 - 50, Screen.height/2 - 30, 200, 60);
 }
 
 function Update() {
 	if (Input.GetKeyDown(KeyCode.Escape) || pressed == true) {
-		pressed = false;
-		showIngameMenu = !showIngameMenu;
-		var player : GameObject = GameObject.FindGameObjectWithTag("Game").GetComponent(GameStatus).player;
-		var status : PlayerStatus = player.GetComponent(PlayerStatus);
-		var motor : CharacterMotorSF;
-		if (status.IsMainPlayer()) {
-			motor = player.GetComponent(CharacterMotorSF);
-			if (!locked) {
-				locked = true;
-				motor.canControl = false;
-			} else {
-				locked = false;
-				motor.canControl = true;
+		if (showChangeMap) {
+			showChangeMap = false;
+			showIngameMenu = true;
+		} else {
+			pressed = false;
+			showIngameMenu = !showIngameMenu;
+			var player : GameObject = GameObject.FindGameObjectWithTag("Game").GetComponent(GameStatus).player;
+			var status : PlayerStatus = player.GetComponent(PlayerStatus);
+			var motor : CharacterMotorSF;
+			if (status.IsMainPlayer()) {
+				motor = player.GetComponent(CharacterMotorSF);
+				if (!locked) {
+					locked = true;
+					motor.canControl = false;
+				} else {
+					locked = false;
+					motor.canControl = true;
+				}
 			}
 		}
+
 
 	}
 }
@@ -33,7 +43,29 @@ function OnGUI () {
 
 	if (showIngameMenu) {
 		ingameMenuRect = GUILayout.Window(0, ingameMenuRect, MakeIngameMenuRect, "Ingame Menu");
+	} else {
+		if (showChangeMap) {
+			changeMapRect = GUILayout.Window(1, changeMapRect, MakeChangeMenuRect, "Change Map");
+		}
 	}
+}
+
+function MakeChangeMenuRect(id : int) {
+	GUILayout.BeginHorizontal();
+	var networkLevel : NetworkLevelLoad = GameObject.FindGameObjectWithTag("Main").GetComponent(NetworkLevelLoad);
+	for (var level in networkLevel.supportedNetworkLevels) {
+		if (GUILayout.Button (level)) {
+			networkLevel.LoadNewLevel(level);
+		}
+	}
+	GUILayout.EndHorizontal();
+	GUILayout.BeginHorizontal();
+	if (GUILayout.Button ("Back to Menu")) {
+		showIngameMenu = true;
+		showChangeMap = false;
+	}
+	GUILayout.EndHorizontal();
+	GUILayout.FlexibleSpace();
 }
 
 function MakeIngameMenuRect(id : int) {
@@ -45,7 +77,8 @@ function MakeIngameMenuRect(id : int) {
 				levelLoad.LoadNewLevel(currentLevel);
 			}
 			if (GUILayout.Button ("Change Map")) {
-				//Do something here.
+				showChangeMap = true;
+				showIngameMenu = false;
 			}
 		}
 		if (GUILayout.Button ("Main Menu / Disconnect")) {
