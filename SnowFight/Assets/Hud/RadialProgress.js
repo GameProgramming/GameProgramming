0489f789becd726e26de5ccb21b58bc42b78e41b
ben <Ben@KeksKlein.(none)>
2012-07-12 05:32:42 +0200

    bigsnowball prefab repariert

--- SnowFight/Assets/Hud/RadialProgress.js
+++ SnowFight/Assets/Hud/RadialProgress.js
@@ -3,17 +3,27 @@ static var progress :float = 0.5;
 static var dominance :float = 0;
 static var icon :Texture;
 
+private var drawIcon :Texture;
+
 function Awake () {
 	progress = 1;
 	dominance = 0;
 	icon = null;
+	drawIcon = null;
 }
 
-function Update () {
+function LateUpdate () {
 	renderer.material.mainTextureOffset = Vector2(progress, 0);
 	progress = 1;
 	dominance = 0;
+	drawIcon = icon;
 	icon = null;
+}
+
+function OnGUI () {
+	if (drawIcon) {
+		
+	}
 }
 
 static function SetRadialProgress (prog :float, dom :float, iconTex :Texture) {
